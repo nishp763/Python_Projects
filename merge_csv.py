bd05b9a50336822be06a25aeddaaf64f8317bafa
nishp763 <nishp763@gmail.com>
2020-12-27 18:12:46 -0500

    solved using pd dataframes

--- merge_csv.py
+++ merge_csv.py
@@ -1,30 +1,20 @@
-''' 
-Algorithm:
-1. check if the file path are valid and file exists
-2. read 1 file and display the output
-3. then merge it into existing one
-4. write to the outfile
-5. close files
-'''
-
 import os # for file path manipulation
 import pandas as pd # for reading csv files
+import numpy as np
 
 def merge_csv(myfilelist, outfile):
+    master_df = pd.DataFrame()
     # check all file which will be read exists and file path is valid
     for file_to_read in myfilelist:
         if (os.path.exists(file_to_read) == False):
             print("ERROR File Doesn't Exist:",file_to_read)
             return
-
-    df1 = pd.read_csv(myfilelist[0])
-    print(df1)
-    df2 = pd.read_csv(myfilelist[1])
-
-    print(df2)
-    # print(myfilelist)
-    # print(outfile)
-    
+        else:
+            df_read = pd.read_csv(file_to_read, header=0)
+            master_df = master_df.append(df_read, ignore_index=True)
+ 
+    master_df.to_csv(outfile, index=False)
+    return
     
 
 if __name__ == "__main__":
