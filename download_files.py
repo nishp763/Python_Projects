8ee9512a6773685c43a1dda2c4d93f236c567a57
nishp763 <nishp763@gmail.com>
2020-12-30 21:42:58 -0500

    progress on download_files.py

--- download_files.py
+++ download_files.py
@@ -1,8 +1,16 @@
 import os # for file directory
 
-def download_files(url, out_folder):
+def download_files(first_url, out_folder):
+    # check if the output directory exists, if not create it
+    if os.path.isdir(out_folder) == False:
+        os.mkdir(out_folder)
+        print('Created directory:',out_folder)
+
+    head, tail = os.path.split(first_url) # head is everything before the slash and tail is everything after the slash
+    print(head,'\t',tail)
     return
 
 if __name__ == "__main__":
     out_path = os.path.join(os.getcwd(),'files','images')
     download_files('http://699340.youcanlearnit.net/image001.jpg',out_path)
+    
